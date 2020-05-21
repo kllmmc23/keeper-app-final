3484342b13b1463231fc3ca1d8f7a31928e07cd3
Michael <63890254+kllmmc23@users.noreply.github.com>
2020-05-20 20:36:04 -0600

    clock

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Header() {
+  setInterval(clock, 1000);
+  const now = new Date().toLocaleTimeString();
+  const [time, setTime] = useState(now);
+  
+  function clock() {
+    const newTime = new Date().toLocaleTimeString();
+
+    setTime(newTime);
+  }
   return (
     <header>
-      <h1>Keeper</h1>
+      <h1>Note Taker App - {time}</h1>
     </header>
   );
 }
